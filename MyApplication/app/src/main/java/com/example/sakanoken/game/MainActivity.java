7e1e6d0432a4dc55dc2b6da2f80f20a35cec4184
Sakano Takeru <sakano@cosmoway.net>
2015-10-16 11:09:31 +0900

    画面遷移の追加

--- MyApplication/app/src/main/java/com/example/sakanoken/game/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sakanoken/game/MainActivity.java
@@ -1,37 +1,37 @@
 package com.example.sakanoken.game;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mCollectionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findviews();
+        init();
+    }
+
+    private void findviews() {
+        mCollectionButton = (Button) findViewById(R.id.collection_button);
+    }
+
+    private void init() {
+        mCollectionButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        Intent intent;
+        if (v == mCollectionButton) {
+            intent = new Intent(this,Collection.class);
+            startActivity(intent);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
