f6c1337a4c5f2b03dd53af3ba48043d169a2c15d
Sakano Takeru <sakano@cosmoway.net>
2015-10-19 15:51:39 +0900

    coinアニメーション

--- MyApplication/app/src/main/java/com/example/sakanoken/game/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sakanoken/game/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.sakanoken.game;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,28 +19,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         findviews();
         init();
-        frameAnimation(this, mCoin);
+        frameAnimation(mCoin);
     }
 
-    void frameAnimation(Context con, View v) {
-        AnimationDrawable anim = new AnimationDrawable();
+    void frameAnimation(View v) {
 
-        // 画像の読み込み //
-        Drawable frame1 = con.getResources().getDrawable(R.drawable.coin1);
-        Drawable frame2 = con.getResources().getDrawable(R.drawable.coin2);
-        Drawable frame3 = con.getResources().getDrawable(R.drawable.coin3);
+        v.setBackgroundResource(R.drawable.coin_animation_item);
 
-        // 画像をアニメーションのコマとして追加していく
-        anim.addFrame(frame1, 600);
-        anim.addFrame(frame2, 600);
-        anim.addFrame(frame3, 600);
-
+        AnimationDrawable anim = (AnimationDrawable) v.getBackground();
 
         // 繰り返し設定
         anim.setOneShot(false);
-
-        // ビューの背景画像にアニメーションを設定
-        v.setBackground(anim);
 
         // アニメーション開始
         anim.start();
