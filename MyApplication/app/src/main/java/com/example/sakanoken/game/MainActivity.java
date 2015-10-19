2ee2c1f8f3e861911f184b0cfa753bafa48aef72
Sakano Takeru <sakano@cosmoway.net>
2015-10-19 17:39:42 +0900

    カプセルのアニメーション追加

--- MyApplication/app/src/main/java/com/example/sakanoken/game/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/sakanoken/game/MainActivity.java
@@ -5,6 +5,9 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -12,8 +15,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button mCollectionButton;
     private ImageView mCoin;
-    private ImageView mcap;
-    private ImageView mrotate;
+    private ImageView mCap;
+    private ImageView mRotate;
+    private ImageView mGacha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +25,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         findviews();
         init();
+        scaleAnimation(mGacha);
         frameAnimation(mCoin);
-        frameAnimation(mcap);
-        frameAnimation(mrotate);
+        frameAnimation(mCap);
+        frameAnimation(mRotate);
+    }
+
+    void scaleAnimation(View v) {
+        AnimationSet set = new AnimationSet(true);
+        ScaleAnimation scale = new ScaleAnimation(1, 2f, 1, 2f); // imgを1倍から2倍に拡大
+        scale.setDuration(3000); // 3000msかけてアニメーションする
+        set.addAnimation(scale);
+        RotateAnimation rotate = new RotateAnimation(0, -360, v.getWidth()/2, v.getHeight()/2); // imgの中心を軸に、0度から360度にかけて回転
+        rotate.setDuration(3000);
+        set.addAnimation(rotate);
+        v.startAnimation(set); // アニメーション適用
     }
 
     void frameAnimation(View v) {
 
         if(v == mCoin) {
             v.setBackgroundResource(R.drawable.coin_animation_item);
-        } else if(v == mcap) {
+        } else if(v == mCap) {
             v.setBackgroundResource(R.drawable.cap_animation_item);
         } else {
           v.setBackgroundResource(R.drawable.rotate_animation_item);
@@ -48,8 +64,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void findviews() {
         mCollectionButton = (Button) findViewById(R.id.collection_button);
         mCoin = (ImageView) findViewById(R.id.coin);
-        mcap = (ImageView) findViewById(R.id.cap);
-        mrotate = (ImageView) findViewById(R.id.rotate);
+        mCap = (ImageView) findViewById(R.id.cap);
+        mRotate = (ImageView) findViewById(R.id.rotate);
+        mGacha = (ImageView) findViewById(R.id.gacha);
     }
 
     private void init() {
